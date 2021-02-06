b29ee1332b5880e6c12099a4431e7d3c7d392d85
Camila Becker <camila.becker01@gmail.com>
2021-02-06 17:54:28 -0300

    Primeiro exercicio de orientação a objeto: Calculadora

---
+++ meu-projeto-cwi-reset/src/oo/Aplicacao.java
@@ -0,0 +1,12 @@
+package oo;
+
+public class Aplicacao {
+    public static void main(String[] args) {
+        Calculadora minhaCalculadora = new Calculadora();
+        System.out.println(minhaCalculadora.soma(10,10));
+        System.out.println(minhaCalculadora.subtrai(10,5));
+        System.out.println(minhaCalculadora.divide(10,2));
+        System.out.println(minhaCalculadora.multiplica(3,3));
+
+    }
+}
---
+++ meu-projeto-cwi-reset/src/oo/Calculadora.java
@@ -0,0 +1,19 @@
+package oo;
+
+public class Calculadora {
+   public int soma(int numero1, int numero2){
+        return numero1 + numero2;
+    }
+
+    public int subtrai(int numero1, int numero2){
+        return numero1 - numero2;
+    }
+
+    public int divide(int numero1, int numero2){
+        return numero1 / numero2;
+    }
+
+    public int multiplica(int numero1, int numero2){
+        return numero1 * numero2;
+    }
+}
