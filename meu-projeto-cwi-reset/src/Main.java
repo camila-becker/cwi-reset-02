204b406f00135d1811dbc2757ac7a8d280b069c0
Camila Becker <camila.becker01@gmail.com>
2021-02-06 16:37:22 -0300

    Alterado a classe Main e adicionado exercicio sobre resto da divisão

--- meu-projeto-cwi-reset/src/Main.java
+++ meu-projeto-cwi-reset/src/Main.java
@@ -1,11 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        int numero1 = 2;
+        int numero1 = 6;
         int numero2 = 3;
-        int resultado;
+        int multiplicacao;
+        int resto;
 
-        resultado = numero1 * numero2;
+        multiplicacao = numero1 * numero2;
+        resto = numero1 % numero2;
 
-        System.out.println(numero1 + " multiplicado por " + numero2 + " é igual a: " + resultado + ".");
+        System.out.println(numero1 + " multiplicado por " + numero2 + " é igual a: " + multiplicacao + ".");
+        System.out.println("O resto da divisão entre " + numero1 + " e " + numero2 + " é: " + resto + ".");
     }
 }
