61e345b472db34b714340b438611191c6773f9be
Camila Becker <camila.becker01@gmail.com>
2021-02-06 16:40:36 -0300

    Alterado a classe Main e adicionado exercicio de comparação entre dois números

--- meu-projeto-cwi-reset/src/Main.java
+++ meu-projeto-cwi-reset/src/Main.java
@@ -4,11 +4,14 @@ public class Main {
         int numero2 = 3;
         int multiplicacao;
         int resto;
+        boolean comparacao;
 
         multiplicacao = numero1 * numero2;
         resto = numero1 % numero2;
+        comparacao = numero1 > numero2;
 
         System.out.println(numero1 + " multiplicado por " + numero2 + " é igual a: " + multiplicacao + ".");
         System.out.println("O resto da divisão entre " + numero1 + " e " + numero2 + " é: " + resto + ".");
+        System.out.println(numero1 + " é maior que " + numero2 + "? " + comparacao + ".");
     }
 }
