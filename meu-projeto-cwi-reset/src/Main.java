65425d224e95c0350b55888bab7fa113516b7191
Camila Becker <camila.becker01@gmail.com>
2021-02-06 16:33:37 -0300

    Alterado a classe Main e adicionado código que faz a multiplicação de dois números

--- meu-projeto-cwi-reset/src/Main.java
+++ meu-projeto-cwi-reset/src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Camila Becker");
+        int numero1 = 2;
+        int numero2 = 3;
+        int resultado;
+
+        resultado = numero1 * numero2;
+
+        System.out.println(numero1 + " multiplicado por " + numero2 + " é igual a: " + resultado + ".");
     }
 }
