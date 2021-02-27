90757671a78f948d7316490b6df6b80812538ab4
Camila Becker <camila.becker01@gmail.com>
2021-02-27 12:27:49 -0300

    correção do exercicio 1

--- aula-04/exemplo-reset/src/main/java/br/com/cwi/reset/exemploreset/NomeController.java
+++ aula-04/exemplo-reset/src/main/java/br/com/cwi/reset/exemploreset/NomeController.java
@@ -3,21 +3,22 @@ package br.com.cwi.reset.exemploreset;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/nome")
 public class NomeController {
     public static String nome = "Camila";
 
-    @GetMapping("/nome")
+    @GetMapping()
     public String nome(){
         return nome;
     }
 
-    @PutMapping("/nome/{nome}")
+    @PutMapping("/{nome}")
     public String mudarNome(@PathVariable("nome") String nome){
         this.nome = nome;
         return this.nome;
     }
 
-    @DeleteMapping("/nome")
+    @DeleteMapping()
     public void deletarNome(){
         this.nome = null;
     }
